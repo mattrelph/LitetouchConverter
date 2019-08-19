809e6e23c08a33b62a5fbc625402086c0c66aa29
Matthew Relph <31970363+mattrelph@users.noreply.github.com>
2019-08-19 16:03:37 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace LitetouchConverter
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.explainLabel = new System.Windows.Forms.Label();
+            this.sourceLabel = new System.Windows.Forms.Label();
+            this.destinationLabel = new System.Windows.Forms.Label();
+            this.sourceFileTextBox = new System.Windows.Forms.TextBox();
+            this.destinationFileTextBox = new System.Windows.Forms.TextBox();
+            this.sourceFileButton = new System.Windows.Forms.Button();
+            this.destinationFileButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.sourceFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.destinationFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.infoTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // explainLabel
+            // 
+            this.explainLabel.AutoSize = true;
+            this.explainLabel.Location = new System.Drawing.Point(13, 13);
+            this.explainLabel.Name = "explainLabel";
+            this.explainLabel.Size = new System.Drawing.Size(513, 13);
+            this.explainLabel.TabIndex = 0;
+            this.explainLabel.Text = "This program converts Liteware 3 (JetDB SQL) and Liteware 4/5/6 (XML) files into " +
+    "JSON files for future use.";
+            this.explainLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // sourceLabel
+            // 
+            this.sourceLabel.AutoSize = true;
+            this.sourceLabel.Location = new System.Drawing.Point(16, 54);
+            this.sourceLabel.Name = "sourceLabel";
+            this.sourceLabel.Size = new System.Drawing.Size(60, 13);
+            this.sourceLabel.TabIndex = 1;
+            this.sourceLabel.Text = "Source File";
+            // 
+            // destinationLabel
+            // 
+            this.destinationLabel.AutoSize = true;
+            this.destinationLabel.Location = new System.Drawing.Point(16, 94);
+            this.destinationLabel.Name = "destinationLabel";
+            this.destinationLabel.Size = new System.Drawing.Size(79, 13);
+            this.destinationLabel.TabIndex = 2;
+            this.destinationLabel.Text = "Destination File";
+            this.destinationLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // sourceFileTextBox
+            // 
+            this.sourceFileTextBox.Location = new System.Drawing.Point(19, 71);
+            this.sourceFileTextBox.Name = "sourceFileTextBox";
+            this.sourceFileTextBox.Size = new System.Drawing.Size(427, 20);
+            this.sourceFileTextBox.TabIndex = 3;
+            this.sourceFileTextBox.TextChanged += new System.EventHandler(this.sourceFileTextBox_TextChanged);
+            // 
+            // destinationFileTextBox
+            // 
+            this.destinationFileTextBox.Location = new System.Drawing.Point(19, 111);
+            this.destinationFileTextBox.Name = "destinationFileTextBox";
+            this.destinationFileTextBox.Size = new System.Drawing.Size(427, 20);
+            this.destinationFileTextBox.TabIndex = 4;
+            this.destinationFileTextBox.TextChanged += new System.EventHandler(this.destinationFileTextBox_TextChanged);
+            // 
+            // sourceFileButton
+            // 
+            this.sourceFileButton.Location = new System.Drawing.Point(465, 67);
+            this.sourceFileButton.Name = "sourceFileButton";
+            this.sourceFileButton.Size = new System.Drawing.Size(75, 23);
+            this.sourceFileButton.TabIndex = 5;
+            this.sourceFileButton.Text = "Browse...";
+            this.sourceFileButton.UseVisualStyleBackColor = true;
+            this.sourceFileButton.Click += new System.EventHandler(this.sourceFileButton_Click);
+            // 
+            // destinationFileButton
+            // 
+            this.destinationFileButton.Location = new System.Drawing.Point(465, 107);
+            this.destinationFileButton.Name = "destinationFileButton";
+            this.destinationFileButton.Size = new System.Drawing.Size(75, 23);
+            this.destinationFileButton.TabIndex = 6;
+            this.destinationFileButton.Text = "Browse...";
+            this.destinationFileButton.UseVisualStyleBackColor = true;
+            this.destinationFileButton.Click += new System.EventHandler(this.destinationFileButton_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Enabled = false;
+            this.convertButton.Location = new System.Drawing.Point(19, 138);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(521, 23);
+            this.convertButton.TabIndex = 7;
+            this.convertButton.Text = "Convert!";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // sourceFileDialog
+            // 
+            this.sourceFileDialog.FileName = "openFileDialog1";
+            this.sourceFileDialog.Filter = "Liteware Files (*.lwd, *.lw4)|*.lwd;*.lw4";
+            // 
+            // destinationFileDialog
+            // 
+            this.destinationFileDialog.CreatePrompt = true;
+            this.destinationFileDialog.DefaultExt = "json";
+            this.destinationFileDialog.Filter = "JSON Files (*.json)|*.json";
+            // 
+            // infoTextBox
+            // 
+            this.infoTextBox.Location = new System.Drawing.Point(19, 168);
+            this.infoTextBox.Multiline = true;
+            this.infoTextBox.Name = "infoTextBox";
+            this.infoTextBox.ReadOnly = true;
+            this.infoTextBox.Size = new System.Drawing.Size(521, 326);
+            this.infoTextBox.TabIndex = 8;
+            this.infoTextBox.Text = "Idle";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 506);
+            this.Controls.Add(this.infoTextBox);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.destinationFileButton);
+            this.Controls.Add(this.sourceFileButton);
+            this.Controls.Add(this.destinationFileTextBox);
+            this.Controls.Add(this.sourceFileTextBox);
+            this.Controls.Add(this.destinationLabel);
+            this.Controls.Add(this.sourceLabel);
+            this.Controls.Add(this.explainLabel);
+            this.Name = "MainWindow";
+            this.Text = "Litetouch Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label explainLabel;
+        private System.Windows.Forms.Label sourceLabel;
+        private System.Windows.Forms.Label destinationLabel;
+        private System.Windows.Forms.TextBox sourceFileTextBox;
+        private System.Windows.Forms.TextBox destinationFileTextBox;
+        private System.Windows.Forms.Button sourceFileButton;
+        private System.Windows.Forms.Button destinationFileButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.OpenFileDialog sourceFileDialog;
+        private System.Windows.Forms.SaveFileDialog destinationFileDialog;
+        private System.Windows.Forms.TextBox infoTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,332 @@
+﻿/* LitewareConverter 0.1
+ * C# - .Net Framework - Windows Forms
+ * Converts Old Liteware files into JSON files. The output will be used on a future project.
+ * 
+ * Accepts Liteware 3  *.lwd (Jet DB SQL) and Liteware 4/5/6 (XML) files and converts them into a plain text JSON object file.
+ * 
+ */
+
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LitetouchConverter
+{
+    public partial class MainWindow : Form
+    {
+        private Boolean debugFlag = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (debugFlag)
+            {
+                this.sourceFileTextBox.Text = "C:\\Users\\myuser\\Documents\\test.lwd";
+                this.destinationFileTextBox.Text = "C:\\Users\\myuser\\Documents\\test.json";
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sourceFileButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.sourceFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.sourceFileTextBox.Text = sourceFileDialog.FileName;
+            }
+        }
+
+        private void destinationFileButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.destinationFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.destinationFileTextBox.Text = destinationFileDialog.FileName;
+            }
+        }
+
+        private void sourceFileTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if ((this.sourceFileTextBox.TextLength != 0) && (this.destinationFileTextBox.TextLength != 0))
+            {
+                this.infoTextBox.Text = "Source and Destination entered: Ready to Convert\r\n";
+                this.convertButton.Enabled = true;
+            }
+            else
+            {
+                this.infoTextBox.Text = "Need Source and Destination to Convert\r\n";
+                this.convertButton.Enabled = false;
+            }
+        }
+
+        private void destinationFileTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if ((this.sourceFileTextBox.TextLength != 0) && (this.destinationFileTextBox.TextLength != 0))
+            {
+                this.infoTextBox.Text = "Source and Destination entered: Ready to Convert\r\n";
+                this.convertButton.Enabled = true;
+            }
+            else
+            {
+                this.infoTextBox.Text = "Need Source and Destination to Convert\r\n";
+                this.convertButton.Enabled = false;
+            }
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            this.infoTextBox.Text=("Starting Conversion...\r\n");
+            //Let's check if the source file exists.
+            string message = "";
+            string caption = "";
+            MessageBoxButtons buttons;
+            DialogResult result;
+            string sourceFileName = sourceFileTextBox.Text;
+            string destinationFileName = destinationFileTextBox.Text;
+            try
+            {
+                if (System.IO.File.Exists(sourceFileName))
+                {
+                    if (debugFlag)
+                    {
+                        this.infoTextBox.AppendText("Source file " + sourceFileName + " does exist.\r\n");
+
+                    }
+                    // Now we check if the destination file is okay
+                    try
+                    {
+                        if (System.IO.File.Exists(destinationFileName))
+                        {
+                            if (debugFlag)
+                            {
+                                this.infoTextBox.AppendText("Destination file " + destinationFileName + " does exist.\r\n");
+
+                            }
+                            this.infoTextBox.AppendText("Writing over destionation file " + destinationFileName + ".\r\n");
+                            try
+                            {
+                                //Delete existing file
+                                System.IO.File.Delete(destinationFileName);
+                            }
+                            catch (Exception error)
+                            {
+                                Console.WriteLine(error.ToString());
+                                this.infoTextBox.Text = "Error clearing destination file.\r\n" + error.ToString() + "\r\nCannot continue.\r\n";
+                                message = "Error clearing destination file.\r\n" + error.ToString() + "\r\nCannot continue.\r\n";
+                                caption = "Error clearing destination file.";
+                                buttons = MessageBoxButtons.OK;
+                                result = MessageBox.Show(message, caption, buttons);
+                                this.infoTextBox.AppendText("Error clearing destination file: " + result.ToString() + "\r\n");
+                                return;
+                            }
+                        }
+                        else if (debugFlag)
+                        {
+                            this.infoTextBox.AppendText("Destination file " + destinationFileName + " does not exist.\r\n");
+                        }
+                    }
+                    catch (Exception error)
+
+                    {
+                        Console.WriteLine(error.ToString());
+                        this.infoTextBox.Text = "Error checking destination file.\r\n" + error.ToString() + "\r\nCannot continue.\r\n";
+                        message = "Error checking destination file.\r\n" + error.ToString() + "\r\nCannot continue.\r\n";
+                        caption = "Error checking destination file.";
+                        buttons = MessageBoxButtons.OK;
+                        result = MessageBox.Show(message, caption, buttons);
+                        this.infoTextBox.AppendText("Error checking destination file: " + result.ToString() + "\r\n");
+                        return;
+                    }
+
+
+
+
+                    //Now we should determine if this is a Liteware 3 file, or Liteware 4 file
+                    //The only filetype check we are going to do for now, is to test the extension of the file. If you botch this, you'll get errors.
+                    //Don't ruin this for me, man
+
+                    if (sourceFileName.EndsWith(".lwd"))
+                    {
+                        this.infoTextBox.AppendText("Liteware 3 (Access / Jet DB SQL) file detected.\r\n");
+                        
+                        if (convertJetDB(sourceFileName, destinationFileName))
+                        {
+                            this.infoTextBox.AppendText("Conversion Complete.\r\n");
+                        }
+                        else
+                        {
+                            this.infoTextBox.AppendText("Conversion Failed.\r\n");
+                        }
+                    }
+                    else if (sourceFileName.EndsWith(".lw4"))
+                    {
+                        this.infoTextBox.AppendText("Liteware 4/5/6 (XML) file detected.\r\n");
+                        if (convertXML(sourceFileName, destinationFileName))
+                        {
+                            this.infoTextBox.AppendText("Conversion Complete.\r\n");
+                        }
+                        else
+                        {
+                            this.infoTextBox.AppendText("Conversion Failed.\r\n");
+                        }
+                    }
+                    else
+                    {
+                        this.infoTextBox.AppendText("File not recognized.\r\nCannot Continue\r\nConversion Failed.\r\n");
+                    }
+
+
+
+
+
+                }
+                else
+                {
+                    this.infoTextBox.Text = "Source file does not exist.\r\n" + sourceFileTextBox.Text + "\r\nCannot continue.\r\n";
+                    message = "Source file does not exist.\r\n" + sourceFileTextBox.Text + "\r\nCannot continue.\r\n";
+                    caption = "Source file does not exist";
+                    buttons = MessageBoxButtons.OK;
+                    result = MessageBox.Show(message, caption, buttons);
+                    this.infoTextBox.AppendText("Error reading source file: " + result.ToString() + "\r\n");
+                }
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.ToString());
+                this.infoTextBox.Text = "Error reading source file.\r\n" + error.ToString()+ "\r\nCannot continue.\r\n";
+                message = "Error reading source file.\r\n" + error.ToString() + "\r\nCannot continue.\r\n";
+                caption = "Error reading source file.";
+                buttons = MessageBoxButtons.OK;
+                result = MessageBox.Show(message, caption, buttons);
+                this.infoTextBox.AppendText("Error reading source file: " + result.ToString() + "\r\n");
+                return;
+
+            }
+
+
+
+        }
+        private Boolean convertXML(string sourceFileName, string destinationFileName)
+        {
+            try
+            {
+                //Read in the raw XML file
+                string rawText = File.ReadAllText(sourceFileName);
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(rawText);
+                //Finally, we convert the dataset into a JSON string and save it into the output file.
+                string json = JsonConvert.SerializeXmlNode(xml, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(destinationFileName, json);
+                return true;
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.ToString());
+                this.infoTextBox.Text = "Error on conversion.\r\n" + error.ToString() + "\r\nCannot continue.\r\n";
+                string message = "Error on conversion.\r\n" + error.ToString() + "\r\nCannot continue.\r\n";
+                string caption = "Error on conversion.";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(message, caption, buttons);
+                this.infoTextBox.AppendText("Error reading source file: " + result.ToString() + "\r\n");
+                return false;
+            }
+            
+        }
+        private Boolean convertJetDB(string sourceFileName, string destinationFileName)
+        {
+            Boolean success = false;
+            this.infoTextBox.AppendText("Opening JetDB Database...\r\n");
+            string message;
+            string caption;
+            MessageBoxButtons buttons;
+            DialogResult result;
+
+            //Open JetDB SQL Database
+            System.Data.OleDb.OleDbConnection conn = new
+            System.Data.OleDb.OleDbConnection();
+            conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+                @"Data source= " + sourceFileName;
+
+            try
+            {
+                conn.Open();
+                this.infoTextBox.AppendText("JetDB Database Opened.\r\n");
+                // Insert code to process data.
+                //Get a list of all tables in DB
+                DataTable userTables = conn.GetSchema("Tables");
+                List<string> tablesList = new List<string>();
+                for (int i = 0; i < userTables.Rows.Count; i++)
+                {
+                    tablesList.Add(userTables.Rows[i][2].ToString());
+                }                
+                string[] tableArray = tablesList.ToArray();
+                DataSet dataSet = new DataSet();
+
+                //Now we retrieve data from each table with a select statement and add that data into the dataset object
+                foreach (string table in tableArray)
+                {
+                    if (debugFlag)
+                    {
+                        this.infoTextBox.AppendText("Processing Table: " + table + "\r\n");
+                    }
+
+                    if ((table != "MSysACEs") && (table != "MSysObjects") && (table != "MSysQueries") && (table != "MSysRelationships")) //These tables give us read errors
+                    {
+                        DataTable dataTable = dataSet.Tables.Add(table);
+                        using (OleDbCommand readRows = new OleDbCommand("SELECT * from " + table, conn))        //Use select command to get all data
+                        {
+                            OleDbDataAdapter adapter = new OleDbDataAdapter(readRows);
+                            adapter.Fill(dataTable);
+                        }
+                    }
+                }
+
+                //Finally, we convert the dataset into a JSON string and save it into the output file.
+                string json = string.Empty;
+                json = JsonConvert.SerializeObject(dataSet, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(destinationFileName, json);
+
+                success = true;
+            }
+            catch (Exception error)
+            {
+                this.infoTextBox.AppendText("Failed to connect to data source. Error reading source file: \r\n" + error.ToString() + "\r\nCannot continue.\r\n");
+                message = "Failed to connect to data source. Error reading source file: \r\n" + error.ToString() + "\r\nCannot continue.\r\n";
+                caption = "Failed to connect to data source.";
+                buttons = MessageBoxButtons.OK;
+                result = MessageBox.Show(message, caption, buttons);
+                this.infoTextBox.AppendText("Error reading source file: " + result.ToString() + "\r\n");
+                success =  false;
+            }
+            finally
+            {
+                conn.Close();
+                this.infoTextBox.AppendText("JetDB Database Closed.\r\n");
+            }
+            return success;
+        }
+    }
+}
